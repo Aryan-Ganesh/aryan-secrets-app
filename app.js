8af33e69a01f24e2c498ad0123592fc3ffb5d35d
Aryan-Ganesh <aryanganesh.aiml@gmail.com>
2022-09-30 20:45:43 -0700

    corrections made in app.js

--- app.js
+++ app.js
@@ -206,5 +206,5 @@ app.get("/logout", (req, res) => {
 })
 // setupServer
 app.listen(3000 || process.env.PORT, () => {
-  console.log(`Server started on port : ${PORT}`);
+  console.log(`Server started on port : 3000`);
 });
